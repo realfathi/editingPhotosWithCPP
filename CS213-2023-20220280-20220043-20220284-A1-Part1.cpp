1c969abdb719ece76ed351ca275d2a43e8bc6035
Muhammad Fathi <147710001+realfathi@users.noreply.github.com>
2023-10-12 13:33:08 +0300

    Update CS213-2023-20220280-20220043-20220284-A1-Part1.cpp

--- CS213-2023-20220280-20220043-20220284-A1-Part1.cpp
+++ CS213-2023-20220280-20220043-20220284-A1-Part1.cpp
@@ -6,7 +6,7 @@ Purpose: Demonstrate use of bmplip for handling  / load a gray image and store i
 Supervised by Dr.Mohammad El-Ramly
 
 Authors:
-Muhammad Ahmed Fathi   -   20220280  -   
+Muhammad Ahmed Fathi   -   20220280  - mohamedfaathy3@gmail.com
 Ahmed Mostafa Muhammad -   20220043  - 05.ahmedmustafa@gmail.com
 Muhammad Gamal ALi     -   20220284  - mohamedgamal200420042004@gmail.com
 
@@ -21,7 +21,7 @@ using namespace std;
 unsigned char image[SIZE][SIZE];
 unsigned char image2[SIZE][SIZE];
 
-int select ; // Global variable 
+char select ; // Global variable
 
 void loadImage ();
 void saveImage ();
@@ -34,23 +34,25 @@ void black_white();
 void flip();
 void darken();
 void lighten();
+void mirror();
 
 //_________________________________________
 
-int menu(){
-    int choice ;
-    choice = -1 ;
-    // this loop check your choice is exist or not 
+char menu(){
+    char choice ;
+    choice = '.' ;
+    // this loop check your choice is exist or not
     // if not chooce again
-    while (choice == -1){
+    while (choice == '.'){
         cout << "Filter 1: Black and White Image\nFilter 2: Invert Image\n";
         cout << "Filter 3: Merge Images\nFilter 4: Flip Image\n";
         cout << "Filter 5: Rotate Image\nFilter 6: Darken and Lighten Image\n";
+        cout << "s: Save the image to a file \n";
         cout << "0: Exit\n";
         cin >> choice ;
-        if (!(choice >= 0 && choice <= 6)){
+        if (!(choice >= '0' && choice <= '6' || choice =='s')){
             cout << "==============\nWrong Number, Please TRY Again !\n\n";
-            choice == -1 ; // loop keep working
+            choice = '.' ; // loop keep working
         }
     }
 
@@ -116,7 +118,7 @@ void invert() {
 
 void rotate_90(){
     /*
-    rotating the image 90 degrees clockwise by switching 
+    rotating the image 90 degrees clockwise by switching
     the rows with the columns and reversing the columns orde
     */
     unsigned char tmp [SIZE][SIZE];
@@ -155,6 +157,7 @@ void black_white(){
                 image[i][j] = 0 ;
         }
     }
+    /*decide if pexel is light or dark then make it black and white*/
 }
 
 //_____________________________________________
@@ -162,13 +165,15 @@ void black_white(){
 void flip(){
 
     int n4 = -1 ;
-    // this loop check your choice is exist or not 
+    // this loop check your choice is exist or not
     // if not chooce again
     while (n4 == -1 ){
         cout<<"1:vertically\n2:horizontally\n";
         cout<<"Enter a number to select horizontally or vertically: ";
         cin>>n4;
+
         if (n4==1){ // vertically
+            /*swapping the values between the top and bottom rows*/
             for (int i = 0; i < SIZE/2; i++) {
                 for (int j = 0; j < SIZE; j++) {
                     int tmp = image[i][j];
@@ -177,6 +182,7 @@ void flip(){
                 }
             }
         }
+            /*swapping the values between the top and bottom column*/
         else if (n4==2){ // horizontally
             for (int i = 0; i < SIZE; i++) {
                 for (int j = 0; j < SIZE/2; j++) {
@@ -196,7 +202,7 @@ void flip(){
 //_____________________________________________
 
 void darken(){
-    // make the image darken by 50% 
+    // make the image darken by 50%
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             image[i][j] /= 2 ;
@@ -207,7 +213,7 @@ void darken(){
 //_____________________________________________
 
 void lighten(){
-    // make the image lighten by 50% 
+    // make the image lighten by 50%
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             if (image[i][j] <= 205)
@@ -221,26 +227,26 @@ void lighten(){
 void megre(){
     for (int i = 0; i < SIZE; i++)
         for (int j = 0; j < SIZE; j++)
-            image[i][j] = (image[i][j] + image2[i][j]) / 2; 
-            // divid by 2 to make pixel between 0 & 255
+            image[i][j] = (image[i][j] + image2[i][j]) / 2;
+    // divid by 2 to make pixel between 0 & 255
 }
 
 //_____________________________________________
 
 void rotate_image(){
     int n5 = -1 ;
-    // this loop check your choice is exist or not 
-    // if not chooce again 
+    // this loop check your choice is exist or not
+    // if not chooce again
     while (n5 == -1 ){
         cout << "1: 90º\n2: 180º\n3: 270º\n";
         cout << "Enter a number to select the angel:\n";
         cin >> n5;
-        if (n5 == 1)  
-            rotate_90(); 
-        else if (n5 == 2)  
-            rotate_180(); 
-        else if (n5 == 3)  
-            rotate_270(); 
+        if (n5 == 1)
+            rotate_90();
+        else if (n5 == 2)
+            rotate_180();
+        else if (n5 == 3)
+            rotate_270();
         else {
             cout << "========\n\nWrong Number, Please TRY Again  *_*\n\n";
             n5 = -1 ; // loop keep working
@@ -251,10 +257,10 @@ void rotate_image(){
 //_____________________________________________
 
 void darken_lighten(){
-    int n6 = -1 ; 
-    // this loop check your choice is exist or not 
-    // if not chooce again 
-    while (n6 == -1 ){ 
+    int n6 = -1 ;
+    // this loop check your choice is exist or not
+    // if not chooce again
+    while (n6 == -1 ){
         cout << "1: Darken Image \n2: Lighten Image \n";
         cout << "Enter a number to select Darken or Lighten Image: ";
         int n6;
@@ -263,63 +269,127 @@ void darken_lighten(){
             darken(); // this to make it darken
             return ;
         }
-        else if (n6 == 2){ 
+        else if (n6 == 2){
             lighten(); // this to make it lighten
             return ;
         }
         else{
             cout << "========\n\nWrong Number, Please TRY Again  *_*\n\n";
-            n6 = -1 ; // loop keep working 
+            n6 = -1 ; // loop keep working
         }
     }
 }
+//_____________________________________________
+void mirror(){
+    cout<<"1:Left 1/2\n2:Right 1/2\n3:Upper 1/2\n4:Lower 1/2.\n";
+    cout<<"Enter a number to select the type of mirroring: ";
+    int n10; cin>>n10;
+    if (n10==1){
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                int tmp = image[i][j];
+                image[i][SIZE-j] = image[i][j];
+            }
+        }
+    }
+    else if (n10==2){
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                int tmp = image[i][j];
+                image[i][j] = image[i][SIZE-j];
+            }
+        }}
+    else if(n10==3){
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                int tmp = image[i][j];
+                image[SIZE -i][j] = image[i][j];
+            }
+        }}
+    else if(n10==4){
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                int tmp = image[i][j];
+                image[i][j] = image[SIZE-i][j];
+            }
+        }}else cout<<"========\n\nWrong Number, Please TRY Again  *_*\n\n";
 
+
+}
 //_____________________________________________
 
 void doSomethingForImage() {
 
-    int num;
-    num = menu() ; // return what we choice from menu 
-    select = num ;
-    switch (num) {
-        case 1:
-            loadImage();
-            black_white();
-            saveImage ();
-            break;
+    char num;
+    while (true) {
+        num = menu() ; // return what we choice from menu
+        select = num ;
+        switch (select) {
+            case '1':
+                loadImage();
+                black_white();
+                break;
 
-        case 2:
-            loadImage();
-            invert();
-            saveImage ();
-            break;
-            
-        case 3:
-            loadImage();
-            // This to add second image it .bmp extension and 
-            //load image to merge with first image 
-            load_to_merge();  
-            megre();
-            saveImage ();
-            break;
+            case '2':
+                loadImage();
+                invert();
+                break;
 
-        case 4:
-            loadImage();
-            flip();
-            saveImage ();
-            break;
+            case '3':
+                loadImage();
+                // This to add second image it .bmp extension and
+                //load image to merge with first image
+                load_to_merge();
+                megre();
+                break;
 
-        case 5:
-            loadImage();
-            rotate_image();
-            saveImage ();
-            break;
+            case '4':
+                loadImage();
+                flip();
+                break;
 
-        case 6:
+            case '5':
+                loadImage();
+                rotate_image();
+                break;
+
+            case '6':
+                loadImage();
+                darken_lighten(); // to make the image darken or lighten
+                break;
+            case '7':
+// code block
+                break;
+            case '8':
+// code block
+                break;
+            case '9':
+// code block
+                break;
+            case '10':
             loadImage();
-            darken_lighten(); // to make the image darken or lighten 
-            saveImage ();
-            break;
+            mirror();
+                break;
+            case '11':
+// code block
+                break;
+            case '12':
+// code block
+                break;
+            case '13':
+// code block
+                break;
+            case '14':
+// code block
+                break;
+            case '15':
+// code block
+                break;
+            case 's':
+                saveImage ();
+                return ;
+                break;
+        }
     }
 }
 
@@ -330,7 +400,7 @@ int main()
 {
     while (true ){
         doSomethingForImage();
-        if (select == 0) // check if you choice from menu 0 the program will end 
+        if (select == '0') // check if you choice from menu 0 the program will end
             return 0;
     }
 }
