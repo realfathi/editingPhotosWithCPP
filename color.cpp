c5a3dfb33ec1146149b225ef16b5f294fc35ca7b
Mohamedgamal235 <131499327+Mohamedgamal235@users.noreply.github.com>
2023-10-15 17:18:54 +0300

    Update color.cpp

--- color.cpp
+++ color.cpp
@@ -1,7 +1,7 @@
 /*
                                             Assignment 1 - OOP
                                                    Part 2
-Date: 13 October 2023
+Date: 15 October 2023
 Purpose: Demonstrate use of bmplip for handling  / load a gray image and store in another file
 Supervised by Dr.Mohammad El-Ramly
 
